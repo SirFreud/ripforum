42f19a24cf51ff51ad02d1e74a7866e6d2dd26a9
Sirfreud <mikevaldez92@gmail.com>
2017-12-15 22:48:08 -0600

    ParticipateInForumTest complete, built addReply method. All tests passing

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -7,14 +7,20 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ParticipateInForum extends TestCase
+class ParticipateInForumTest extends TestCase
 {
+    use DatabaseMigrations;
     /** @test */
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
         // Given we have an authenticated user and an existing thread
+        $this->be($user = factory('App\User')->create());
+        $thread = factory('App\Thread')->create();
         // When the user adds a reply to the thread
+        $reply = factory('App\Reply')->create();
+        $this->post('/threads/' . $thread->id . '/replies', $reply->toArray());
         // Their reply should be visible on the page 
+        $this->get($thread->path())
+            ->assertSee($reply->body);
     }
-    
 }
