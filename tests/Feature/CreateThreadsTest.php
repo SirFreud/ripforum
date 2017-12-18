7b56338037173364aaf51644f2c0fadad198ebf3
Sirfreud <mikevaldez92@gmail.com>
2017-12-18 03:43:07 -0600

    Fixed TestCase class to selectively enable exception handling

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -20,6 +20,15 @@ class CreateThreadsTest extends TestCase
     }
 
     /** @test */
+    public function guests_can_not_see_the_create_threads_page()
+    {
+        $this->withExceptionHandling()
+            ->get('/threads/create')
+            ->assertRedirect('/login');
+    }
+    
+
+    /** @test */
     public function an_authenticated_user_can_create_new_forum_threads()
     {
         // Given we have a signed in user
